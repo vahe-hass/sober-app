db835fe72a46499b941f7827ddf79d2e9d31f92f
vahe-hass <info@vahegrickoryhassratian.com>
2024-10-18 15:02:31 +0400

    Dashboard screen edited

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class DashboardScreen extends StatefulWidget {
@@ -8,18 +9,10 @@ class DashboardScreen extends StatefulWidget {
 
 class _DashboardScreenState extends State<DashboardScreen> {
   // PayPal donation link
-  final String _donationUrl = 'https://www.paypal.com/donate?hosted_button_id=YOUR_BUTTON_ID';
+  final String _donationUrl =
+      'https://www.paypal.com/donate?hosted_button_id=YOUR_BUTTON_ID';
 
-  // Function to launch the URL
-  Future<void> _launchDonationUrl() async {
-    if (await canLaunch(_donationUrl)) {
-      await launch(_donationUrl);
-    } else {
-      throw 'Could not launch $_donationUrl';
-    }
-  }
-
-  int soberDays = 0; // Placeholder for the number of sober days
+  int soberDays = 0;
 
   // Define achievements with their respective unlock days
   final List<Map<String, dynamic>> achievements = [
@@ -30,12 +23,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
     {'text': 'Six months sober', 'daysRequired': 180},
     {'text': 'One year sober', 'daysRequired': 365},
   ];
-
-  @override
-  void initState() {
-    super.initState();
-    // Normally you'd load soberDays here, e.g., from SharedPreferences
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,46 +45,102 @@ class _DashboardScreenState extends State<DashboardScreen> {
                 bool isUnlocked = soberDays >= achievement['daysRequired'];
 
                 return Card(
-                  margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  margin: const EdgeInsets.symmetric(
+                      vertical: 8.0, horizontal: 16.0),
                   color: Colors.white,
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.emoji_events,
-                      color: isUnlocked ? Colors.green : Colors.grey,  // Highlight icon based on unlock status
-                      size: 40,
-                    ),
-                    title: Text(
-                      achievement['text'],
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: isUnlocked ? Colors.black : Colors.grey,  // Grey out text if not unlocked
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.emoji_events,
+                        color: isUnlocked
+                            ? Theme.of(context).primaryColorLight
+                            : Colors
+                                .grey, // Highlight icon based on unlock status
+                        size: 60,
                       ),
-                    ),
-                    subtitle: Text(
-                      '${achievement['daysRequired']} days required',
-                      style: const TextStyle(fontSize: 14),
+                      title: Text(
+                        achievement['text'],
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: 'Poppins',
+                          color: isUnlocked
+                              ? Theme.of(context).primaryColorLight
+                              : Colors.grey, // Grey out text if not unlocked
+                        ),
+                      ),
+                      subtitle: Text(
+                        '${achievement['daysRequired']} days required',
+                        style: const TextStyle(
+                          fontFamily: 'Noto-Sans',
+                          fontSize: 14,
+                        ),
+                      ),
                     ),
                   ),
                 );
               },
             ),
           ),
-          // Donate Button at the bottom
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20.0),
-            child: ElevatedButton(
-              onPressed: _launchDonationUrl,
-              child: const Text(
-                'Donate',
-                style: TextStyle(
-                  fontFamily: 'Roboto',
-                  color: Colors.black,
-                ),
+          const Text(
+            'Support Our Mission',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Poppins',
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              'Your donation helps us improve Sober and continue supporting more people on their journey to sobriety. Every contribution makes a difference!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontFamily: 'Noto-Sans',
               ),
             ),
           ),
+          // Donate Button at the bottom
+          ElevatedButton(
+            onPressed: _launchDonationUrl,
+            child: const Text(
+              'Donate',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                color: Colors.black,
+              ),
+            ),
+          ),
+          SizedBox(height: 15)
         ],
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
+
+  // Function to launch the URL
+  Future<void> _launchDonationUrl() async {
+    if (await canLaunch(_donationUrl)) {
+      await launch(_donationUrl);
+    } else {
+      throw 'Could not launch $_donationUrl';
+    }
+  }
+
+  // Function to load user data (soberDays) from local storage
+  Future<void> _loadUserData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String? dateString = prefs.getString('sobrietyDate');
+    if (dateString != null) {
+      DateTime sobrietyStartDate = DateTime.parse(dateString);
+      setState(() {
+        soberDays = DateTime.now().difference(sobrietyStartDate).inDays;
+      });
+    }
   }
 }
